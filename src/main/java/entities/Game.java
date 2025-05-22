00bcfb6b1bd33d22f8e8ea8cc8dba20dfd19d8e9
Rafael Santos <fael3000net.rcs@gmail.com>
2025-05-21 21:35:08 -0300

    Game seed

--- src/main/java/entities/Game.java
+++ src/main/java/entities/Game.java
@@ -16,22 +16,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
  public Game(){
 
  }
 
-    public Game(String title, Long id, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(String title, Long id, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
 
         this.title = title;
         this.id = id;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -77,12 +83,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getShortDescription() {
